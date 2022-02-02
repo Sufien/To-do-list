e4f154c3a9003c2d5218f109231c4b341d8eee62
Soufiane <soufianetai@hotmail.it>
2022-02-02 15:11:12 +0100

    Starting project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+//import textBar from './components/textBar';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
